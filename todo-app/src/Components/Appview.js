a9c5827d0b438a2df7cff9474b08b6847113c341
brandford7 <brandfordjunior@gmail.com>
2021-09-18 15:11:08 +0000

    init

---
+++ todo-app/src/Components/Appview.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Appview = () => {
+    return (
+        <div className="container">
+            <div className="app-container">
+            <header>
+                <h1>My Todo List</h1>
+
+                </header>
+                </div>
+        </div>
+    )
+};
+
+export default Appview ;

c14df93abf3cc6a08fb04b4ce2884b44153e3957
brandford7 <brandfordjunior@gmail.com>
2021-09-18 15:06:32 +0000

    ../todo-app/src/Components/

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import Appview from './Components/Appview';
+import setSearchterm from './redux/setSearchterm' ;
+import { clearSearchterm } from './redux/clearSearchterm';
+import { removeRecipe } from './redux/removeRecipe'
+import {Provider} from 'react-redux' ;
+import {Store} from './redux/Store'
+
 
 function App() {
   return (
+    <Provider store={Store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <Store store={Store}/>
+      
     </div>
+     
+      </Provider> 
+    
   );
 }
 
